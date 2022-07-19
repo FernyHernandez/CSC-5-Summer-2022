b9325944a076b2ee2af65512fcc78e4cdfc45f4a
Ferny Hernandez <100494588+KyDevv@users.noreply.github.com>
2022-07-18 21:32:00 -0700

    Add files via upload

---
+++ Class/CPPTemplate/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: Ferny Hernandez
+ * Created on June 22, 2022, 7:11 PM
+ * Purpose: Creation of Template to be used for all
+ *           future projects
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants, no Global Variables are allowed
+//Math/Physics/Conversions/Higher Dimensions - i.e. PI, e, etc...
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Set the random number seed
+    
+    //Declare Variables
+    
+    
+    //Initialize or input i.e. set variable values
+   
+    
+    //Map inputs -> outputs
+    
+    
+    //Display the outputs
+  
+
+    //Exit stage right or left!
+    return 0;
+}
