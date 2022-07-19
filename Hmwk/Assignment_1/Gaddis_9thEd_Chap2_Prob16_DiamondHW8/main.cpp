cece5dfb08c71772318c626a67d670014b7fe29f
Ferny Hernandez <100494588+KyDevv@users.noreply.github.com>
2022-07-18 20:57:40 -0700

    Add files via upload

---
+++ Hmwk/Assignment_1/Gaddis_9thEd_Chap2_Prob16_DiamondHW8/main.cpp
@@ -0,0 +1,42 @@
+/* 
+ * File:   main.cpp
+ * Author: Ferny Hernandez
+ * Created on June 29, 2022, 4:00 PM
+ * Purpose: Homework Problem 8 / Diamond Pattern
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Mathematical/Physics/Conversions, Higher dimensioned arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    
+    //Initialize the Random Number Seed
+     
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map inputs to outputs -> The Process
+    
+    //Display Results
+    cout << "   *" << endl;
+    cout << "  ***" << endl;
+    cout << " *****" << endl;
+    cout << "*******" << endl;
+    cout << " *****" << endl;
+    cout << "  ***" << endl;
+    cout << "   *" << endl;
+    
+    //Exit stage right
+    return 0;
+}
+
